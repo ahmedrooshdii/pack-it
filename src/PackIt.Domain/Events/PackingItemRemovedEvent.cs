0523d4ae70b17558cb0f7f886c09515c782e6ddb
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-12 22:44:22 +0200

    feat(domain): add domain events (Added, Packed, Removed)

---
+++ src/PackIt.Domain/Events/PackingItemRemovedEvent.cs
@@ -0,0 +1,8 @@
+﻿using PackIt.Domain.Entities;
+using PackIt.Domain.ValueObjects.PackingItem;
+using PackIt.Shared.Abstractions.Domain;
+
+namespace PackIt.Domain.Events
+{
+    public record PackingItemRemovedEvent(PackingList PackingList, PackingItem PackingItem) : IDomainEvent;
+}
