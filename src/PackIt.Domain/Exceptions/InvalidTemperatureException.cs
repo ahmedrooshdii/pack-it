28d5dc9509c1158ca5c6ab30917fb63d4051b13f
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-13 06:09:04 +0200

    feat(domain): add InvalidTemperature and InvalidTravelDays exceptions

---
+++ src/PackIt.Domain/Exceptions/InvalidTemperatureException.cs
@@ -0,0 +1,14 @@
+﻿using PackIt.Shared.Abstractions.Exceptions;
+
+namespace PackIt.Domain.Exceptions
+{
+    public class InvalidTemperatureException : PackItException
+    {
+        public double Temperature { get; }
+        public InvalidTemperatureException(double temperature)
+            : base($"Value '{temperature}' is invalid temperature.")
+        {
+            Temperature = temperature;
+        }
+    }
+}
