37927e9f0cf2970504a3ffeb9c495a0d07d8605e
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-13 06:09:29 +0200

    feat(domain): add Temperature and TravelDays value objects

---
+++ src/PackIt.Domain/ValueObjects/PackingList/Temperature.cs
@@ -0,0 +1,25 @@
+﻿using PackIt.Domain.Exceptions;
+
+namespace PackIt.Domain.ValueObjects.PackingList
+{
+    public record Temperature
+    {
+        public double Value { get; }
+
+        public Temperature(double value)
+        {
+            if (value is < -100 or > 100)
+            {
+                throw new InvalidTemperatureException(value);
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator double(Temperature temperature)
+            => temperature.Value;
+
+        public static implicit operator Temperature(double value)
+            => new Temperature(value);
+    }
+}
---
+++ src/PackIt.Domain/ValueObjects/PackingList/TravelDayes.cs
@@ -0,0 +1,24 @@
+﻿using PackIt.Domain.Exceptions;
+
+namespace PackIt.Domain.ValueObjects.PackingList
+{
+    public record TravelDays
+    {
+        public ushort Value { get; }
+        public TravelDays(ushort value)
+        {
+            if (value is 0 or > 90)
+            {
+                throw new InvalidTravelDaysException(value);
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator ushort(TravelDays travelDays)
+            => travelDays.Value;
+
+        public static implicit operator TravelDays(ushort value)
+            => new TravelDays(value);
+    }
+}
