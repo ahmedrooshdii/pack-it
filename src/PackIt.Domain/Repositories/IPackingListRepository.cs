55fe16e210e3c5a28a76ecffdcc8ca45d526dbc7
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-13 06:11:21 +0200

    feat(domain): add IPackingListRepository interface

---
+++ src/PackIt.Domain/Repositories/IPackingListRepository.cs
@@ -0,0 +1,13 @@
+﻿using PackIt.Domain.Entities;
+using PackIt.Domain.ValueObjects.PackingList;
+
+namespace PackIt.Domain.Repositories
+{
+    public interface IPackingListRepository
+    {
+        Task<PackingList> GetAsync(PackingListId id);
+        Task AddAsync(PackingList packingList);
+        Task UpdateAsync(PackingList packingList);
+        Task DeleteAsync(PackingListId id);
+    }
+}
