715c9d01dbdf239c48c7894918b0c56cee1cac7a
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-13 06:10:41 +0200

    feat(domain): add High and Low temperature policies

---
+++ src/PackIt.Domain/Policies/Temprature/HighTemperaturePolicy.cs
@@ -0,0 +1,19 @@
+﻿using PackIt.Domain.Policies;
+using PackIt.Domain.ValueObjects.PackingItem;
+
+namespace PackIt.Domain.Policies.Temprature
+{
+    internal sealed class HighTemperaturePolicy : IPackingItemsPolicy
+    {
+        public bool IsApplicable(PolicyData data)
+            => data.Temperature > 25D;
+
+        public IEnumerable<PackingItem> GenerateItems(PolicyData data)
+            => new List<PackingItem>
+            {
+            new PackingItem("Light T-Shirt", 2),
+            new PackingItem("Sunglasses", 1),
+            new PackingItem("Cream with UV filter", 1)
+            };
+    }
+}
---
+++ src/PackIt.Domain/Policies/Temprature/LowTemperaturePolicy.cs
@@ -0,0 +1,21 @@
+﻿using PackIt.Domain.Policies;
+using PackIt.Domain.ValueObjects.PackingItem;
+
+namespace PackIt.Domain.Policies.Temprature
+{
+    internal sealed class LowTemperaturePolicy : IPackingItemsPolicy
+    {
+        public bool IsApplicable(PolicyData data)
+            => data.Temperature < 10D;
+        public IEnumerable<PackingItem> GenerateItems(PolicyData data)
+            => new List<PackingItem>
+            {
+            new PackingItem("Warm Jacket", 1),
+            new PackingItem("Sweater", 2),
+            new PackingItem("Scarf", 1),
+            new PackingItem("Gloves", 1),
+            new PackingItem("Warm jacket", 1),
+            new PackingItem("Thermal Underwear", 2)
+            };
+    }
+}
