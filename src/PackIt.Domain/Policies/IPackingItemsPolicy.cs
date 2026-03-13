49bc512581ca25c7aae07e4cd7366b82e4f4311d
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-13 06:10:08 +0200

    feat(domain): add IPackingItemsPolicy interface and PolicyData

---
+++ src/PackIt.Domain/Policies/IPackingItemsPolicy.cs
@@ -0,0 +1,10 @@
+﻿using PackIt.Domain.ValueObjects.PackingItem;
+
+namespace PackIt.Domain.Policies
+{
+    public interface IPackingItemsPolicy
+    {
+        bool IsApplicable(PolicyData data);
+        IEnumerable<PackingItem> GenerateItems(PolicyData data);
+    }
+}
---
+++ src/PackIt.Domain/Policies/PolicyData.cs
@@ -0,0 +1,6 @@
+﻿using PackIt.Domain.ValueObjects.PackingList;
+
+namespace PackIt.Domain.Policies
+{
+    public record PolicyData(TravelDays Days, Consts.Gender Gender, Temperature Temperature, Localization Localization);
+}
