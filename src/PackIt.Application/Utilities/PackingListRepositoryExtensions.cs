11306fab42dafe3af362ef2f696f0abdf31b1d47
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-14 08:26:23 +0200

    feat(application): add GetOrThrowAsync extension on IPackingListRepository

---
+++ src/PackIt.Application/Utilities/PackingListRepositoryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using PackIt.Application.Exceptions;
+using PackIt.Domain.Entities;
+using PackIt.Domain.Repositories;
+
+namespace PackIt.Application.Utilities
+{
+    public static class PackingListRepositoryExtensions
+    {
+        public static async Task<PackingList> GetOrThrowAsync(
+            this IPackingListRepository repository,
+            Guid packingListId)
+        {
+            var packingList = await repository.GetAsync(packingListId);
+
+            if (packingList is null)
+            {
+                throw new PackingListNotFoundException(packingListId);
+            }
+
+            return packingList;
+        }
+    }
+}
