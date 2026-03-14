4fc46c81ba58bbdd7d8e6be152a1826bb7698a64
ahmedroshdii <ahmed.roshdi@circle360.net>
2026-03-14 06:12:39 +0200

    feat(application): add AddApplication extension with commands and policy registration

---
+++ src/PackIt.Application/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PackIt.Domain.Factories;
+using PackIt.Domain.Policies;
+using PackIt.Shared;
+
+namespace PackIt.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddCommands();
+            services.AddSingleton<IPackingListFactory, PackingListFactory>();
+
+            services.Scan(s => s.FromAssemblies(typeof(IPackingListFactory).Assembly)
+                .AddClasses(c => c.AssignableTo(typeof(IPackingItemsPolicy)))
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime()); // Because I hardcode policies, I can use singleton lifetime
+
+            return services;
+        }
+    }
+}
